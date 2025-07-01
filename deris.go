507be4db6b91e9566273a69e4507e003abd9b238
Himank Dave <himank.dave40@gmail.com>
2025-07-02 00:27:25 +0300

    feat: add GET, SET and DEL commands
    Signed-off-by: Himank Dave <himank.dave40@gmail.com>

--- deris.go
+++ deris.go
@@ -4,14 +4,48 @@ import (
 	"bufio"
 	"fmt"
 	"net"
+	"strings"
 )
+
+func handleError(c net.Conn, errInfo string) {
+	errStr := fmt.Sprintf("ERROR: %s", errInfo)
+	fmt.Println(errStr)
+	fmt.Fprintln(c, errStr)
+}
 
 func handleConnection(conn net.Conn) {
 	scanner := bufio.NewScanner(conn)
 	for scanner.Scan() {
-		input := scanner.Text()
-		fmt.Printf("got ping... [message='%s']\n", input)
-		fmt.Fprintln(conn, "Hello World")
+		line := scanner.Text()
+		fmt.Printf("got ping... [cmd='%s']\n", line)
+
+		args := strings.Fields(line)
+		if len(args) == 0 {
+			handleError(conn, "empty command")
+			continue
+		}
+		cmd := strings.ToUpper(args[0])
+
+		switch cmd {
+		case "GET":
+			if len(args[1:]) != 1 {
+				handleError(conn, "invalid number of arguments")
+				continue
+			}
+			fmt.Fprintf(conn, "%s\n", args[1])
+		case "SET":
+			if len(args[1:]) != 2 {
+				handleError(conn, "invalid number of arguments")
+				continue
+			}
+			fmt.Fprintf(conn, "%s-%s\n", args[1], args[2])
+		case "DEL":
+			if len(args[1:]) != 1 {
+				handleError(conn, "invalid number of arguments")
+				continue
+			}
+			fmt.Fprintf(conn, "%s\n", args[1])
+		}
 	}
 }
 
