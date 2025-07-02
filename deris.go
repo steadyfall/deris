ca84403931cab180d06d02a71dd3ca474060c38f
Himank Dave <himank.dave40@gmail.com>
2025-07-03 00:16:12 +0300

    feat: implement key-value store for GET, SET, and DEL commands
    Signed-off-by: Himank Dave <himank.dave40@gmail.com>

--- deris.go
+++ deris.go
@@ -13,7 +13,7 @@ func handleError(c net.Conn, errInfo string) {
 	fmt.Fprintln(c, errStr)
 }
 
-func handleConnection(conn net.Conn) {
+func handleConnection(conn net.Conn, kv map[string]string) {
 	scanner := bufio.NewScanner(conn)
 	for scanner.Scan() {
 		line := scanner.Text()
@@ -27,29 +27,47 @@ func handleConnection(conn net.Conn) {
 		cmd := strings.ToUpper(args[0])
 
 		switch cmd {
+		// cmd: GET <key>
 		case "GET":
 			if len(args[1:]) != 1 {
 				handleError(conn, "invalid number of arguments")
 				continue
 			}
-			fmt.Fprintf(conn, "%s\n", args[1])
+			val, present := kv[args[1]]
+			if !present {
+				handleError(conn, fmt.Sprintf("key '%s' not present", args[1]))
+				continue
+			}
+			fmt.Fprintf(conn, "%s\n", val)
+
+		// cmd: SET <key> <value>
 		case "SET":
 			if len(args[1:]) != 2 {
 				handleError(conn, "invalid number of arguments")
 				continue
 			}
-			fmt.Fprintf(conn, "%s-%s\n", args[1], args[2])
+			// TODO: Make SET operation immutable
+			key, val := args[1], args[2]
+			kv[key] = val
+
+		// cmd: DEL <key>
 		case "DEL":
 			if len(args[1:]) != 1 {
 				handleError(conn, "invalid number of arguments")
 				continue
 			}
-			fmt.Fprintf(conn, "%s\n", args[1])
+			_, present := kv[args[1]]
+			if !present {
+				handleError(conn, fmt.Sprintf("key '%s' not present", args[1]))
+				continue
+			}
+			delete(kv, args[1])
 		}
 	}
 }
 
 func main() {
+	db := make(map[string]string)
 	lis, err := net.Listen("tcp", ":6969")
 	if err != nil {
 		panic(err)
@@ -60,6 +78,6 @@ func main() {
 		if err != nil {
 			continue
 		}
-		go handleConnection(conn)
+		go handleConnection(conn, db)
 	}
 }
