688a69567b6f38e6f7187d0637ebe060227d7c9e
Himank Dave <himank.dave40@gmail.com>
2025-07-03 23:16:38 +0300

    feat: implement data persistence with snapshot log
    Signed-off-by: Himank Dave <himank.dave40@gmail.com>

--- deris.go
+++ deris.go
@@ -1,19 +1,39 @@
-package main
+package deris
 
 import (
 	"bufio"
 	"fmt"
+	"io"
 	"net"
+	"os"
 	"strings"
 )
 
-func handleError(c net.Conn, errInfo string) {
-	errStr := fmt.Sprintf("ERROR: %s", errInfo)
-	fmt.Println(errStr)
-	fmt.Fprintln(c, errStr)
+func loadSnapshot(kv map[string]string, logFile string) {
+	fmt.Printf("loading data from %s :)\n", logFile)
+	snapshot, _ := os.Open(logFile)
+	scanner := bufio.NewScanner(snapshot)
+	for scanner.Scan() {
+		line := scanner.Text()
+		args := strings.Fields(line)
+		cmd := strings.ToUpper(args[0])
+		switch cmd {
+		case "SET":
+			key, val := args[1], args[2]
+			kv[key] = val
+		case "DEL":
+			delete(kv, args[1])
+		}
+	}
 }
 
-func handleConnection(conn net.Conn, kv map[string]string) {
+func handleError(w io.Writer, errInfo string) {
+	errStr := fmt.Sprintf("ERROR: %s", errInfo)
+	fmt.Println(errStr)
+	fmt.Fprintln(w, errStr)
+}
+
+func handleConnection(conn net.Conn, kv map[string]string, logFile io.Writer) {
 	scanner := bufio.NewScanner(conn)
 	for scanner.Scan() {
 		line := scanner.Text()
@@ -49,6 +69,7 @@ func handleConnection(conn net.Conn, kv map[string]string) {
 			// TODO: Make SET operation immutable
 			key, val := args[1], args[2]
 			kv[key] = val
+			fmt.Fprintf(logFile, fmt.Sprintf("%s %s %s\n", args[0], args[1], args[2]))
 
 		// cmd: DEL <key>
 		case "DEL":
@@ -62,6 +83,7 @@ func handleConnection(conn net.Conn, kv map[string]string) {
 				continue
 			}
 			delete(kv, args[1])
+			fmt.Fprintf(logFile, fmt.Sprintf("%s %s\n", args[0], args[1]))
 
 		case "EXIT":
 			fmt.Fprintln(conn, "-- Quitting session...")
@@ -72,6 +94,13 @@ func handleConnection(conn net.Conn, kv map[string]string) {
 
 func StartServer(port uint64) {
 	db := make(map[string]string)
+
+	if _, err := os.Stat("snapshot.log"); err == nil {
+		loadSnapshot(db, "snapshot.log")
+	} else if os.IsNotExist(err) {
+		fmt.Println("snapshot not available :|")
+	}
+	snapshot, _ := os.OpenFile("snapshot.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
 
 	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", port))
 	if err != nil {
@@ -84,7 +113,7 @@ func StartServer(port uint64) {
 		if err != nil {
 			continue
 		}
-		go handleConnection(conn, db)
+		go handleConnection(conn, db, snapshot)
 	}
 }
 
