1400d0c0363670351e149786e2c6f72453540954
Himank Dave <himank.dave40@gmail.com>
2025-07-25 17:27:28 -0400

    feat(cmd): add --port flag for configurable server port

--- cmd/deris/main.go
+++ cmd/deris/main.go
@@ -2,9 +2,13 @@ package main
 
 import (
 	"github.com/steadyfall/deris"
+	"flag"
 )
 
 func main() {
-	var srvPort uint64 = 6969
+	var srvPort uint64
+	flag.Uint64Var(&srvPort, "port", 6969, "port to listen on")
+	flag.Parse()
+	
 	deris.StartServer(srvPort, "snapshot.log")
 }
